8058a415c71f5e44f1a4abee21446ed1ab2ea6e8
adityajoshigit <adityasjoshi96@gmail.com>
2023-01-21 16:37:02 -0500

    segregated feedbacks data into a separate folder

--- src/App.js
+++ src/App.js
@@ -1,51 +1,24 @@
+import { useState } from "react";
+import FeedbackItem from "./components/FeedbackItem";
 import Header from "./components/Header";
+import feedbacks from "./data/feedbackData";
 
 function App() {
 
-  const allFeedbacks = [
-    {
-      id: 1,
-      rating: 10,
-      description: 'Informative content, undoubtedly one of the best courses I have ever followed. Thanks!',
-      author: {
-        firstName: 'John',
-        lastName: 'Doe'
-      }
-    },
-    {
-      id: 2,
-      rating: 8,
-      description: 'Good, mid size course for beginners and intermediate developers. Would like to know more about advanced courses.',
-      author: {
-        firstName: 'Jane',
-        lastName: 'Doe'
-      }
-    },
-    {
-      id: 3,
-      rating: 6,
-      description: 'A little difficult for me as I am not very well versed with ES6+ features. I was expecting to learn those in this course but it is not included. Otherwise, a decent course.',
-      author: {
-        firstName: 'Ronald',
-        lastName: 'McGill'
-      }
-    },
-    {
-      id: 4,
-      rating: 7,
-      description: 'Good first course to start your React JS journey with. Still expecting some advanced level content. However, I like the fact that it is practical course with demonstrations.',
-      author: {
-        firstName: 'Ben',
-        lastName: 'Grover'
-      }
-    },
-  ];
+  const [allFeedbacks, setAllFeedbacks] = useState(feedbacks);
 
   return (
-    <div className='container'>
+    <>
       <Header headerTitle={'Any feedback for me..?'}/>
-      
-    </div>
+      <div className='container'>
+        <FeedbackItem item={{id: 1, description: 'something', rating: 10}} />
+        {/* {
+          allFeedbacks.forEach(feedbackItem => {
+            return <FeedbackItem item={feedbackItem} key={feedbackItem.id} />;
+          })
+        } */}
+      </div>
+    </>
   );
 }
 
---
+++ src/data/feedbackData.js
@@ -0,0 +1,40 @@
+const feedbacks = [
+    {
+      id: 1,
+      rating: 10,
+      description: 'Informative content, undoubtedly one of the best courses I have ever followed. Thanks!',
+      author: {
+        firstName: 'John',
+        lastName: 'Doe'
+      }
+    },
+    {
+      id: 2,
+      rating: 8,
+      description: 'Good, mid size course for beginners and intermediate developers. Would like to know more about advanced courses.',
+      author: {
+        firstName: 'Jane',
+        lastName: 'Doe'
+      }
+    },
+    {
+      id: 3,
+      rating: 6,
+      description: 'A little difficult for me as I am not very well versed with ES6+ features. I was expecting to learn those in this course but it is not included. Otherwise, a decent course.',
+      author: {
+        firstName: 'Ronald',
+        lastName: 'McGill'
+      }
+    },
+    {
+      id: 4,
+      rating: 7,
+      description: 'Good first course to start your React JS journey with. Still expecting some advanced level content. However, I like the fact that it is practical course with demonstrations.',
+      author: {
+        firstName: 'Ben',
+        lastName: 'Grover'
+      }
+    }
+  ];
+
+export default feedbacks;
