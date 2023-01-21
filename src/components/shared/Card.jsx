61e84a3d2756ba178c5c24e67e9e2a9ae25aa3a5
adityajoshigit <adityasjoshi96@gmail.com>
2023-01-21 16:55:22 -0500

    prop-type configuration

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function Card({
     children,
@@ -10,5 +11,14 @@ function Card({
     </div>
   )
 }
+
+Card.propTypes = {
+    children: PropTypes.node,
+    reverse: PropTypes.bool
+};
+
+Card.defaultProps = {
+    reverse: false
+};
 
 export default Card
