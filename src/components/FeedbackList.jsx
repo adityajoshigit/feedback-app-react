66488aeaabeb5dd1ee99e322be76565c807443e8
adityajoshigit <adityasjoshi96@gmail.com>
2023-01-22 13:26:07 -0500

    added some animations and router dom pkg for routing

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types'
+import { motion, AnimatePresence } from 'framer-motion';
 
 function FeedbackList({
     feedbacks,
@@ -8,22 +9,51 @@ function FeedbackList({
 }) {
   return (
     <div className='feedback-list'>
-      {   
-        (feedbacks && feedbacks.filter(item => !item.isDeleted).length) 
-        ? (
-          feedbacks
-            .filter(item => !item.isDeleted )
-            .map(item => (
-              <FeedbackItem 
-                key={item.id} 
-                item={item} 
-                handleFeedbackDelete={handleFeedbackDelete}/>
-            ) )
-        )
-        : 'No feedbacks yet..'
-      }
+      <AnimatePresence>
+        {   
+          (feedbacks && feedbacks.filter(item => !item.isDeleted).length) 
+          ? (
+            feedbacks
+              .filter(item => !item.isDeleted )
+              .map(item => (
+                <motion.div 
+                  key={item.id}
+                  initial={ { opacity: 0 } }
+                  animate={ { opacity: 1 } }
+                  exit={ { opacity: 0 } } 
+                >
+                  <FeedbackItem 
+                    key={item.id} 
+                    item={item} 
+                    handleFeedbackDelete={handleFeedbackDelete}
+                  />
+                </motion.div>
+              ) )
+          )
+          : 'No feedbacks yet..'
+        }
+      </AnimatePresence>
     </div>
   )
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {   
+  //       (feedbacks && feedbacks.filter(item => !item.isDeleted).length) 
+  //       ? (
+  //         feedbacks
+  //           .filter(item => !item.isDeleted )
+  //           .map(item => (
+  //             <FeedbackItem 
+  //               key={item.id} 
+  //               item={item} 
+  //               handleFeedbackDelete={handleFeedbackDelete}/>
+  //           ) )
+  //       )
+  //       : 'No feedbacks yet..'
+  //     }
+  //   </div>
+  // )
 }
 
 FeedbackList.defaultProps = {
